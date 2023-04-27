78934ca318ade953e0f892c7a195240eb7ece1f3
Christopher L <56335217+chrisalee@users.noreply.github.com>
2023-04-27 07:50:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -59,3 +59,57 @@ function activityNotifications(expenditure, d) {
 
     return notifications;
 }
+
+/////////////////////////////////////////////////////////////////////
+function activityNotifications(expenditure, d) {
+    // algorithm using counting sort
+    
+    let notificationsSent = 0;
+    
+    const allExp = new Array(201).fill(0);  // each room represents the expenditure costs 
+    
+    // in allExp room, all expenditures are sorted.  so finding a value which is not 0 will be the median value.  median can be 2 if the values are even number
+    const medianCount1 = Math.floor((d + 1) / 2);
+    const medianCount2 = Math.ceil((d + 1) / 2);
+    
+    const findMedianExp = medianCount1 === medianCount2 ? () => {
+        // if only one median
+        let median = medianCount1, v = 0, maxVal = allExp.length;
+        for(let v = 0; v < allExp.length; v++) {
+            median -=allExp[v];
+            if(median <= 0) return v * 2;
+        }
+    } : () => {
+        // if 2 medians, the second median will be found next turn after first median
+        let median = medianCount1, ret = 0;
+        for(let v = 0; v < allExp.length; v++) {
+            median -=allExp[v];
+            if(median == 0) {
+                ret = v;  // one more turn
+            } else if(median < 0) {
+                if(ret === 0) {
+                    return v * 2;
+                }
+                else {
+                    return ret + v;
+                }
+            }
+        }
+    }
+    let day = 0;
+    // fill first d-1 expenditures into the counting sorted array
+    for(; day < d; day++) {
+        allExp[expenditure[day]]++;  // fill counts of expenditures of day
+    }
+    // move forward next days
+    let noticeThreshold;
+    for(; day < expenditure.length; day++) {
+        noticeThreshold = findMedianExp();
+        if(noticeThreshold <= expenditure[day]) notificationsSent++;
+        
+        allExp[expenditure[day]]++; // increase count of the day
+        allExp[expenditure[day - d]]-- // reduce count of the past day from the trailing
+    }
+    console.log(notificationsSent, '**')
+    return notificationsSent;
+}
